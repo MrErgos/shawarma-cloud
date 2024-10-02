bc05ca9ee8b91b02aca58a932f245fcf9dda460a
George <babin.svet.v@gmail.com>
2024-10-03 02:17:28 +0300

    Initial commit

---
+++ src/main/java/com/springstudy/shawarma_cloud/ShawarmaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.shawarma_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShawarmaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShawarmaCloudApplication.class, args);
+	}
+
+}
